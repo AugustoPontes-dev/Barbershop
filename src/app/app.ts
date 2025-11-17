6fa2c08d60320056fc0407f3a52dcf05ea124e62
augusto <Augustoponte0808@gmail.com>
2025-11-17 18:03:52 -0300

    formulario

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./header/header";
 import { Calendario } from "./calendario/calendario";
+import { FormularioReserva } from "./formulario-reserva/formulario-reserva";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Calendario],
+  imports: [RouterOutlet, Header, Calendario, FormularioReserva],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
