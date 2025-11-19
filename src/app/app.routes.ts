ac432c9abefe7f333796ee6527d5e8032f74bcde
augusto <Augustoponte0808@gmail.com>
2025-11-19 13:14:03 -0300

    pastas docs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,8 @@
 import { Routes } from '@angular/router';
 import { Reserva } from './pages/reserva/reserva';
-import { Contato } from './contato/contato';
 import { Chat } from './pages/chat/chat';
 
 export const routes: Routes = [
-<<<<<<< HEAD
-{path: '', component: Reserva},
-=======
 {path: '1', component: Reserva},
 {path: '', component: Chat},
->>>>>>> c479328365ea238f5548a22dfc4145fb308cb1ea
 ];
