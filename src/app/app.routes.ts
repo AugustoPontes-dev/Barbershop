ed44219dd3178eff6211f3ddd7332fcfb303da7f
augusto <Augustoponte0808@gmail.com>
2025-11-25 04:04:06 -0300

    ajuste paginas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { Reserva } from './pages/reserva/reserva';
 import { Chat } from './pages/chat/chat';
 import { Servico } from './pages/servico/servico';
+import { Sobre } from './pages/sobre/sobre';
 
 export const routes: Routes = [
 {path: '', component: Reserva},
 {path: 'chat', component: Chat},
 {path: 'servico', component: Servico},
+{path: 'sobre', component: Sobre},
 ];
